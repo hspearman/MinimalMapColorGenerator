ac157e2d6e9441803218a4550c2e5b61ad7f3d5a
Hannah Spearman <hannah.spearman@saggezza.com>
2014-11-16 12:38:48 -0600

    second pass

--- Province.py
+++ Province.py
@@ -1,33 +1,53 @@
 class Province:
     def get_color(self):
         return self._color
+
     def set_color(self, color):
         self._color = color
+
     def __del_color(self):
         del self._color
 
     # Color of the province
-    _color = property(get_color, set_color, __del_color)
+    color = property(get_color, set_color, __del_color)
 
     def get_neighbors(self):
-        return self._color
-    def __set_neighbors(self, neighbors):
+        return self._neighbors
+
+    def set_neighbors(self, neighbors):
         self._neighbors = neighbors
+
     def __del_neighbors(self):
         del self._neighbors
 
     # List of provinces that share a border with this province
-    _neighbors = property(get_neighbors, __set_neighbors, __del_neighbors)
+    neighbors = property(get_neighbors, set_neighbors, __del_neighbors)
 
     def get_visited(self):
         return self._visited
-    def __set_visited(self, visited):
+
+    def set_visited(self, visited):
         self._visited = visited
+
     def __del_visited(self):
         del self._visited
 
     # Flags whether or not this node has been visited
-    _visited = property(get_visited, __set_visited, __del_visited)
+    visited = property(get_visited, set_visited, __del_visited)
 
-    def __init__(self):
-        self._visited = False
+    def get_name(self):
+        return self._name
+
+    def __set_name(self, name):
+        self._name = name
+
+    def __del_name(self):
+        del self._name
+
+    # Flags whether or not this node has been visited
+    name = property(get_name, __set_name, __del_name)
+
+    def __init__(self, name):
+        self.visited = False
+        self.name = name
+        self.color = None
--- Test.py
+++ Test.py
@@ -1,1 +1,113 @@
+import random
+from Province import Province
+
+hex_color_characters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+available_colors = []
+
+
 def main():
+
+    # Initialize provinces
+    a = Province("a")
+    b = Province("b")
+    c = Province("c")
+    d = Province("d")
+    e = Province("e")
+    f = Province("f")
+    g = Province("g")
+    h = Province("h")
+
+    # Set neighbors
+    a.set_neighbors([b, e, d])
+    b.set_neighbors([a, c, d, e, f])
+    c.set_neighbors([b, f, h])
+    d.set_neighbors([a, e, g])
+    e.set_neighbors([a, b, d, f, g, h])
+    f.set_neighbors([b, c, e, h])
+    g.set_neighbors([d, e, h])
+    h.set_neighbors([c, f, e, g])
+
+    provinces = [a, b, c, d, e, f, g, h]
+
+    # Run the algorithm
+    process_provinces(provinces)
+
+    # Print out the results
+    print("Colors of provinces:")
+    for province in provinces:
+        print(province.get_name() + ": " + province.get_color())
+    print("")
+
+    if is_valid_solution(provinces):
+        print("Solution is valid!")
+    else:
+        print("Solution is invalid!")
+
+
+def process_provinces(provinces):
+
+    # loop through provinces
+    for province in provinces:
+        calculate_color(province)
+
+
+def calculate_color(province):
+
+        # If we already calculate the color, return it
+        visited = province.get_visited()
+        if visited:
+            return province.get_color()
+
+        # Otherwise, calculate it
+        else:
+            province.set_visited(True)
+
+            colors_of_neighbors = []
+
+            # Get the colors of the province's neighbors
+            neighbors = province.get_neighbors()
+            for neighbor in neighbors:
+                colors_of_neighbors.append(calculate_color(neighbor))
+
+            # Choose a color that no neighboring province use
+            for color in available_colors:
+                if color not in colors_of_neighbors:
+                    province.set_color(color)
+
+            # If no unique color is available, generate a new one
+            if None == province.get_color():
+                color = generate_color()
+                province.set_color(color)
+                available_colors.append(color)
+
+            return province.get_color()
+
+
+def is_valid_solution(provinces):
+
+    is_valid = True
+
+    max_neighbors = 0
+
+    # Loop through all the provinces
+    for province in provinces:
+        province_color = province.get_color()
+
+        neighbors = province.get_neighbors()
+        num_of_neighbors = len(neighbors)
+        if num_of_neighbors > max_neighbors:
+            max_neighbors = num_of_neighbors
+
+        # Make sure each province doesn't have the same color as any bordering provinces
+        for neighbor in neighbors:
+            if neighbor.get_color() == province_color:
+                is_valid = False
+                break
+
+    return is_valid
+
+
+def generate_color():
+    return "#" + ''.join(random.sample(hex_color_characters, 6))
+
+main()
